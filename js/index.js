41eb3e5414eb44b642f822a859808f7f0cbd74ae
Istvan Acs <istvan.acs89@gmail.com>
2020-01-19 20:19:34 +0000

    Add some javascript for the menu and styling

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', removeOpen);
+});
+
+function removeOpen() {
+  document.body.classList.remove('nav-open');
+}
